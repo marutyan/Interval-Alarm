6f1c99472a1d4486a9c6aa0ee719cc1802023d08
marutyan <132348090+marutyan@users.noreply.github.com>
2025-06-25 01:48:39 +0900

    XMLレイアウトを利用し、アラーム設定値（開始・終了時刻、間隔）を取得する処理を追加

--- app/src/main/java/com/example/intervalalarm/MainActivity.kt
+++ app/src/main/java/com/example/intervalalarm/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.intervalalarm
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TimePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -17,6 +21,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val startTimePicker = findViewById<TimePicker>(R.id.startTimePicker)
+        val endTimePicker = findViewById<TimePicker>(R.id.endTimePicker)
+        val intervalEditText = findViewById<EditText>(R.id.intervalEditText)
+        val setAlarmButton = findViewById<Button>(R.id.setAlarmButton)
+
+        setAlarmButton.setOnClickListener {
+            val startHour = startTimePicker.hour
+            val startMinute = startTimePicker.minute
+            val endHour = endTimePicker.hour
+            val endMinute = endTimePicker.minute
+            val intervalText = intervalEditText.text.toString()
+            val intervalMinutes = intervalText.toIntOrNull() ?: 0
+
+            Log.d("IntervalAlarm", "開始時刻: $startHour:$startMinute")
+            Log.d("IntervalAlarm", "終了時刻: $endHour:$endMinute")
+            Log.d("IntervalAlarm", "間隔: $intervalMinutes 分")
+        }
+
         setContent {
             IntervalAlarmTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
